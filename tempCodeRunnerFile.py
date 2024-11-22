8e56f8a545cd63cd9b831877a6dbd0285caef85a
Mihir-pp <mihirpat.09@gmail.com>
2024-11-22 17:02:15 +0530

    made changes in UI

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
